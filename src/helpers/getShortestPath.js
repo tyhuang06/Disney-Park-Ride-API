f20ae9c6e2430843de3e2d329b34dde280541bd6
Jamie Huang <tyhuang06@gmail.com>
2022-08-02 15:17:02 -0400

    fix(request): take in json data from request

--- src/helpers/getShortestPath.js
+++ src/helpers/getShortestPath.js
@@ -1,17 +1,19 @@
 import { getAllRides, getDistanceToAll } from './fetchMap.js';
 import { BellmanFord } from './bellmanFord.js';
 
-const getShortestPath = async (source) => {
+const getShortestPath = async (sourceId, algo) => {
 	// get all rides and exclude not opened ones
 	let rides = await getAllRides();
 	rides = rides.filter((ride) => ride.is_open);
 
 	// generate graph
 	// default source to first ride
-	if (!source) {
-		source = rides[0];
+	if (!sourceId) {
+		sourceId = rides[0].id;
 	}
-	const sourceIdx = rides.findIndex((ride) => ride.id === source.id);
+
+	const source = rides.find((ride) => ride.id === sourceId);
+	const sourceIdx = rides.findIndex((ride) => ride.id === sourceId);
 
 	rides = await getDistanceToAll(source, rides);
 
@@ -26,9 +28,13 @@ const getShortestPath = async (source) => {
 		});
 	});
 
-	// find shortest path using Bellman-Ford
-	const bf = new BellmanFord(sourceIdx, rides, edges);
-	const shortestWeight = bf.run();
+	let shortestWeight = [];
+
+	if (algo === 'bellman-ford') {
+		// find shortest path using Bellman-Ford
+		const bf = new BellmanFord(sourceIdx, rides, edges);
+		shortestWeight = bf.run();
+	}
 
 	// add first ride's wait time
 	shortestWeight[sourceIdx] += source.wait_time;
