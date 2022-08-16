bceda1a113a7f718c8841cf6b4dfc6cd3b73a643
Jamie Huang <tyhuang06@gmail.com>
2022-08-16 15:19:42 -0400

    fix(default): update default algo as Dijkstra

--- src/controllers/visitOrderController.js
+++ src/controllers/visitOrderController.js
@@ -2,7 +2,7 @@ import getShortestPath from '../helpers/getShortestPath.js';
 
 const getVisitOrder = async (req, res) => {
 	const sourceId = req.body.sourceId;
-	const algo = req.body.algo || 'bellman-ford';
+	const algo = req.body.algo || 'dijkstra';
 
 	const rides = await getShortestPath(sourceId, algo);
 
