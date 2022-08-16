0e777393d7c1d582ae0ed9e3d545199f87286d7e
szeyireinachan <szeyic@gmail.com>
2022-08-16 16:38:42 -0400

    fix(dijkstra): added comments

--- src/helpers/dijkstra.js
+++ src/helpers/dijkstra.js
@@ -10,23 +10,30 @@ export class Dijkstra {
 		this.dist = Array(vertices.length).fill(Infinity);
 		this.dist[source] = 0;
 
-		// this.adjacencyList = {};
+		// heap comparator: compare by weight
 		const comparator = (a, b) => a.weight - b.weight;
 
+		// create min heap
 		this.minHeap = new Heap(comparator);
 		this.minHeap.init(this.edges);
 	}
 
 	run() {
+		// create visited set
 		const visited = new Set();
 
+		// loop through minHeap
 		while (this.minHeap.length > 0) {
+
+			// get edge with smallest weight
 			const smallest = this.minHeap.pop();
 
+			// check if smallest weight is visited
 			if (visited.has(smallest)) {
 				continue;
 			}
 
+			// update target distance if necessary
 			for (let i = 0; i < this.vertices.length; i++) {
 				if (
 					this.dist[i] >
@@ -37,6 +44,7 @@ export class Dijkstra {
 				}
 			}
 
+			// add smallest weight to visited
 			visited.add(smallest);
 		}
 
